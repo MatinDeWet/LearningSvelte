a615a4a433b4735694ac580530a8f69532c73831
Matin de Wet <matindewet116@gmail.com>
2024-08-17 08:24:13 +0200

    gotcha 1

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,22 +1,31 @@
 <script>
-	let count = 0;
+	let count1 = 0;
+	let count2 = 0;
 
-	$: if (count > 5) {
-		console.log(`The count is ${count}`);
+	// not going to work variables needs to be present on the right hand side of a decleration.
+	$: string = `Count total is ${getTotel()}`;
+
+	function getTotel() {
+		return count1 + count2;
 	}
 
-	$: string = `You have clicked ${count} times.`;
-
-	const increment = () => {
-		count += 1;
-	};
+	function increment1() {
+		count1 += 1;
+	}
+	function increment2() {
+		count2 += 1;
+	}
 </script>
 
-<button on:click={increment}>
-	Clicks {count}
+<button on:click={increment1}>
+	Clicks {count1}
 </button>
+
+<button on:click={increment2}>
+	Clicks {count2}
+</button>
+
 <h3>{string}</h3>
-<h3>You have clicked {count} times.</h3>
 
 <style>
 	button {
