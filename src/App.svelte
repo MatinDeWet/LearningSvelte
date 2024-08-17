ae60ad02618e17ea31dcd599252253ba0729dcaa
Matin de Wet <matindewet116@gmail.com>
2024-08-17 08:00:37 +0200

    basic structures

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	let name = 'Svelte';
+	let string = 'Some <strong>string</strong> with html';
+</script>
+
+<h1>Hello {name.toLocaleLowerCase()}!</h1>
+<p>{@html string}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
