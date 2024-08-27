2c0a0e6a8ddc87e08203b7ecfc85ce40ece80ff6
Matin de Wet <matindewet116@gmail.com>
2024-08-27 17:35:43 +0200

    styling for todo list scss

--- src/App.svelte
+++ src/App.svelte
@@ -1,87 +1,78 @@
 <script>
-	import TodoList from "./lib/TodoList.svelte";
-	import { v4 as uuid} from "uuid"
-	import { tick } from "svelte";
-
+	import TodoList from './lib/TodoList.svelte';
+	import { v4 as uuid } from 'uuid';
+	import { tick } from 'svelte';
+  
 	let todoList;
 	let showList = true;
-
-	let todos =[
+  
+	let todos = [
+	  {
+		id: uuid(),
+		title: 'Todo 1',
+		completed: true
+	  },
+	  {
+		id: uuid(),
+		title: 'Todo 2',
+		completed: false
+	  },
+	  {
+		id: uuid(),
+		title: 'Todo 3',
+		completed: true
+	  },
+	  {
+		id: uuid(),
+		title:
+		  'A long long long long long long long long long long long long long long long long long long todo',
+		completed: false
+	  }
+	];
+  
+	async function handleAddTodo(event) {
+	  event.preventDefault();
+	  todos = [
+		...todos,
 		{
-			id: uuid(),
-			title : 'Todo 1',
-			completed: true
-		},
-		{
-			id: uuid(),
-			title : 'Todo 2',
-			completed: true
-		},
-		{
-			id: uuid(),
-			title : 'Todo 3',
-			completed: false
-		},
-		{
-			id: uuid(),
-			title : 'Todo 4',
-			completed: false
-		}        
-	]
-
-	async function handleAddTodo(event){
-		event.preventDefault();
-		console.log(document.querySelectorAll('.todo-list ul li'))
-
-		todos = [...todos,{
-			id: uuid(),
-			title: event.detail.title,
-			completed: false
-		}];
-
-		await tick();
-
-		console.log(document.querySelectorAll('.todo-list ul li'))
-		todoList.clearInput();
+		  id: uuid(),
+		  title: event.detail.title,
+		  completed: false
+		}
+	  ];
+	  await tick();
+	  todoList.clearInput();
 	}
-
-	function handleRemoveTodo(event){
-		todos = todos.filter(t => t.id !== event.detail.id)
+  
+	function handleRemoveTodo(event) {
+	  todos = todos.filter((t) => t.id !== event.detail.id);
 	}
-
-	function handleToggleTodo(event){
-		todos = todos.map(t => {
-			if(t.id === event.detail.id){
-				return {...t, completed: event.detail.value}
-			}else{
-				return t;
-			}
-		})
+  
+	function handleToggleTodo(event) {
+	  todos = todos.map((todo) => {
+		if (todo.id === event.detail.id) {
+		  return { ...todo, completed: event.detail.value };
+		}
+		return { ...todo };
+	  });
 	}
-</script>
-
-<label>
+  </script>
+  
+  <label>
 	<input type="checkbox" bind:checked={showList} />
 	Show/Hide list
-</label>
-
-{#if showList}
-	<div style:max-width="200px">
-		<TodoList 
-		{todos} 
+  </label>
+  {#if showList}
+	<div style:max-width="400px">
+	  <TodoList
+		{todos}
 		bind:this={todoList}
 		on:addtodo={handleAddTodo}
 		on:removetodo={handleRemoveTodo}
 		on:toggletodo={handleToggleTodo}
-		>
-
-		</TodoList>
+	  />
 	</div>
-{/if}
-
-
-
-<button on:click={() => { todoList.focusInput()}}>Focus input</button>
-
-<style>
-</style>
+  {/if}
+  
+  <style>
+  </style>
