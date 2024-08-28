051754bf84ff128992a45954e9e43d051306c48b
Matin de Wet <matindewet116@gmail.com>
2024-08-28 14:15:35 +0200

    delete todos

--- src/App.svelte
+++ src/App.svelte
@@ -2,89 +2,105 @@
 	import TodoList from './lib/TodoList.svelte';
 	import { v4 as uuid } from 'uuid';
 	import { tick, onMount } from 'svelte';
-  
+
 	let todoList;
 	let showList = true;
-  
+
 	let todos = null;
 	let error = null;
 	let isLoading = false;
 	let isAdding = false;
-  
+	let disabledItems = [];
+
 	onMount(() => {
-	  loadTodos();
+		loadTodos();
 	});
-  
+
 	async function loadTodos() {
-	  isLoading = true;
-	  await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10').then(async (response) => {
-		if (response.ok) {
-		  todos = await response.json();
-		} else {
-		  error = 'An error has occurred';
-		}
-	  });
-	  isLoading = false;
+		isLoading = true;
+		await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10').then(
+			async (response) => {
+				if (response.ok) {
+					todos = await response.json();
+				} else {
+					error = 'An error has occurred';
+				}
+			}
+		);
+		isLoading = false;
 	}
-  
+
 	async function handleAddTodo(event) {
-	  event.preventDefault();
-	  isAdding = true;
-	  await fetch('https://jsonplaceholder.typicode.com/todos', {
-		method: 'POST',
-		body: JSON.stringify({
-		  title: event.detail.title,
-		  completed: false
-		}),
-		headers: {
-		  'Content-type': 'application/json; charset=UTF-8'
-		}
-	  }).then(async (response) => {
-		if (response.ok) {
-		  const todo = await response.json();
-		  todos = [...todos, { ...todo, id: uuid() }];
-		  todoList.clearInput();
-		} else {
-		  alert('An error has occurred.');
-		}
-	  });
-	  isAdding = false;
-	  await tick();
-	  todoList.focusInput();
+		event.preventDefault();
+		isAdding = true;
+		await fetch('https://jsonplaceholder.typicode.com/todos', {
+			method: 'POST',
+			body: JSON.stringify({
+				title: event.detail.title,
+				completed: false
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			}
+		}).then(async (response) => {
+			if (response.ok) {
+				const todo = await response.json();
+				todos = [...todos, { ...todo, id: uuid() }];
+				todoList.clearInput();
+			} else {
+				alert('An error has occurred.');
+			}
+		});
+		isAdding = false;
+		await tick();
+		todoList.focusInput();
 	}
-  
-	function handleRemoveTodo(event) {
-	  todos = todos.filter((t) => t.id !== event.detail.id);
+
+	async function handleRemoveTodo(event) {
+		const id = event.detail.id;
+		if (disabledItems.includes(id)) return;
+		disabledItems = [...disabledItems, id];
+		await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+			method: 'DELETE'
+		}).then((response) => {
+			if (response.ok) {
+				todos = todos.filter((t) => t.id !== event.detail.id);
+			} else {
+				alert('An error has occurred.');
+			}
+		});
+		disabledItems = disabledItems.filter((itemId) => itemId !== id);
 	}
-  
+
 	function handleToggleTodo(event) {
-	  todos = todos.map((todo) => {
-		if (todo.id === event.detail.id) {
-		  return { ...todo, completed: event.detail.value };
-		}
-		return { ...todo };
-	  });
+		todos = todos.map((todo) => {
+			if (todo.id === event.detail.id) {
+				return { ...todo, completed: event.detail.value };
+			}
+			return { ...todo };
+		});
 	}
-  </script>
-  
-  <label>
+</script>
+
+<label>
 	<input type="checkbox" bind:checked={showList} />
 	Show/Hide list
-  </label>
-  {#if showList}
+</label>
+{#if showList}
 	<div style:max-width="400px">
-	  <TodoList
-		{todos}
-		{error}
-		{isLoading}
-		disableAdding={isAdding}
-		bind:this={todoList}
-		on:addtodo={handleAddTodo}
-		on:removetodo={handleRemoveTodo}
-		on:toggletodo={handleToggleTodo}
-	  />
+		<TodoList
+			{todos}
+			{error}
+			{isLoading}
+			{disabledItems}
+			disableAdding={isAdding}
+			bind:this={todoList}
+			on:addtodo={handleAddTodo}
+			on:removetodo={handleRemoveTodo}
+			on:toggletodo={handleToggleTodo}
+		/>
 	</div>
-  {/if}
-  
-  <style>
-  </style>
+{/if}
+
+<style>
+</style>
