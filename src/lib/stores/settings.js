d232012336eec6922b55dce36b822a78be825ff7
Matin de Wet <matindewet116@gmail.com>
2024-08-31 10:12:43 +0200

    writable store callback functions

--- src/lib/stores/settings.js
+++ src/lib/stores/settings.js
@@ -1,9 +1,24 @@
 import { writable } from 'svelte/store';
 
-const settings = writable({
-	colorScheme: 'dark',
-	fontSize: 16,
-	languege: 'en'
-});
+const settings = writable(
+	{
+		colorScheme: 'dark',
+		fontSize: 16,
+		languege: 'en'
+	},
+	(set) => {
+		const timer = setTimeout(() => {
+			set({
+				colorScheme: 'light',
+				fontSize: 16,
+				languege: 'en'
+			});
+		}, 1000);
+		console.log('from 0 to 1');
+		return () => {
+			clearTimeout(timer);
+		};
+	}
+);
 
 export default settings;
