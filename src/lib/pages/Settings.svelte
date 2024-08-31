fe22ac0dd89d91ee8ebea449f02c8745d4730fbd
Matin de Wet <matindewet116@gmail.com>
2024-08-31 11:04:26 +0200

    derived stores

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,5 +1,6 @@
 <script>
 	import settings from '../stores/settings';
+	import direction from '../stores/direction';
 </script>
 
 <h2>Settings</h2>
@@ -27,12 +28,23 @@
 </button>
 <br />
 
-{$settings.colorScheme}
+Direction: {$direction}
 <br />
-{$settings.language}
+
+Color Scheme: {$settings.colorScheme}
+<br />
+Language: {$settings.language}
 <br />
 
 <label>
 	<input type="radio" name="colorScheme" value="dark" bind:group={$settings.colorScheme} /> Dark
 	<input type="radio" name="colorScheme" value="light" bind:group={$settings.colorScheme} /> Light
 </label>
+
+<br />
+
+<select bind:value={$settings.language}>
+	{#each ['en', 'ar'] as lang}
+		<option value={lang} selected={$settings.language === lang}>{lang}</option>
+	{/each}
+</select>
